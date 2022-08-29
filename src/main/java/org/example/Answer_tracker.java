56367aca47275b1b107180d933d7cab3751e703e
Zulqarnayeen <xperimentschaftler@gmail.com>
2022-08-29 07:10:01 +0600

    Initial commit

---
+++ src/main/java/org/example/Answer_tracker.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Answer_tracker extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent e){
+
+    }
+}
---
+++ src/main/java/org/example/Emoji_guessing.java
@@ -0,0 +1,6 @@
+package org.example;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Emoji_guessing extends ListenerAdapter {
+}
